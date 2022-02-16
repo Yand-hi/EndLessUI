a0aedf019e23f6809bf5a5a3c3e91fca97bf9049
Yandhi <15877410869@163.com>
2022-02-16 20:41:21 +0800

    Skeleton2.demo.vue done

---
+++ src/components/skeletons/Skeleton2.demo.vue
@@ -0,0 +1,15 @@
+<demo>
+rows 自定义行数
+</demo>
+
+<template>
+  <Skeleton :rows="6"/>
+</template>
+
+<script lang="ts">
+import Skeleton from '../../lib/Skeleton.vue'
+
+export default {
+  components: {Skeleton},
+};
+</script>
--- src/components/skeletons/SkeletonDemo.vue
+++ src/components/skeletons/SkeletonDemo.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>Skeleton 骨架图</h1>
     <Demo :component="Skeleton1Demo"/>
+    <Demo :component="Skeleton2Demo"/>
     <SkeletonAttrs/>
   </div>
 </template>
 
 <script lang="ts">
 import Skeleton1Demo from "./Skeleton1.demo.vue"
+import Skeleton2Demo from "./Skeleton2.demo.vue"
 import SkeletonAttrs from './SkeletonAttrs.vue'
 import Demo from "../Demo.vue";
 
@@ -19,6 +21,7 @@ export default {
   setup() {
     return {
       Skeleton1Demo,
+      Skeleton2Demo
     };
   },
 };
